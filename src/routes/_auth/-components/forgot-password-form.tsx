feb10b299c83111e497120bb95f30f068a9dd91f
stemca <stevenmatthew.arce@gmail.com>
2025-11-20 11:10:26 -0800

    add success message

--- src/routes/_auth/-components/forgot-password-form.tsx
+++ src/routes/_auth/-components/forgot-password-form.tsx
@@ -2,6 +2,7 @@ import { useForm } from "@tanstack/react-form";
 import { useMutation } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import { useServerFn } from "@tanstack/react-start";
+import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
@@ -16,7 +17,8 @@ import { forgotPasswordFn } from "../-api/forgot-password";
 import { ForgotPasswordSchema } from "../-schemas/forgot-password";
 
 export default function ForgotPasswordForm() {
-  const { isPending, mutateAsync } = useMutation({
+  const [email, setEmail] = useState("");
+  const { isPending, mutateAsync, isSuccess } = useMutation({
     mutationFn: useServerFn(forgotPasswordFn),
   });
 
@@ -31,7 +33,7 @@ export default function ForgotPasswordForm() {
       try {
         await mutateAsync({ data: value });
         form.reset();
-
+        setEmail(value.email);
         toast.success("Reset email password sent");
       } catch (error) {
         if (error instanceof Error) {
@@ -51,61 +53,71 @@ export default function ForgotPasswordForm() {
           alt="MillenniCare logo"
           className="w-2/3 flex-1"
         />
+        {isSuccess ? (
+          <p className="flex flex-1 items-center justify-center text-center text-gray-500">
+            An email has been sent to {email} with instructions on how to reset
+            your password.
+          </p>
+        ) : (
+          <>
+            <p className="flex-1 text-center text-gray-500">
+              Forgot your password? Request a reset password link by entering
+              your email below.
+            </p>
 
-        <p className="flex-1 text-center text-gray-500">
-          Forgot your password? Request a reset password link by entering your
-          email below.
-        </p>
-
-        <form
-          className="h-full w-full flex-1 space-y-3"
-          onSubmit={(e) => {
-            e.preventDefault();
-            form.handleSubmit();
-          }}
-        >
-          <FieldGroup>
-            <form.Field
-              name="email"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched && !field.state.meta.isValid;
-                return (
-                  <Field data-invalid={isInvalid}>
-                    <FieldLabel htmlFor={field.name}>Email Address</FieldLabel>
-                    <Input
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                      aria-invalid={isInvalid}
-                      placeholder="Enter your email"
-                      autoComplete="email"
-                      type="email"
-                    />
-                    {isInvalid && (
-                      <FieldError errors={field.state.meta.errors} />
-                    )}
-                  </Field>
-                );
+            <form
+              className="h-full w-full flex-1 space-y-3"
+              onSubmit={(e) => {
+                e.preventDefault();
+                form.handleSubmit();
               }}
-            />
-          </FieldGroup>
+            >
+              <FieldGroup>
+                <form.Field
+                  name="email"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid;
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>
+                          Email Address
+                        </FieldLabel>
+                        <Input
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          aria-invalid={isInvalid}
+                          placeholder="Enter your email"
+                          autoComplete="email"
+                          type="email"
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    );
+                  }}
+                />
+              </FieldGroup>
 
-          <Button type="submit" disabled={isPending} className="w-full">
-            {isPending ? (
-              <>
-                <Spinner />
-                Submitting...
-              </>
-            ) : (
-              "Submit"
-            )}
-          </Button>
-        </form>
+              <Button type="submit" disabled={isPending} className="w-full">
+                {isPending ? (
+                  <>
+                    <Spinner />
+                    Submitting...
+                  </>
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </form>
+          </>
+        )}
 
-        <Button asChild variant="link">
+        <Button asChild variant="link" className="flex-1">
           <Link to="/sign-in">Back to sign in</Link>
         </Button>
       </div>
