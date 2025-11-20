0089bb38791c47e50d4858c6da80fb0546d058a6
stemca <stevenmatthew.arce@gmail.com>
2025-11-20 10:58:08 -0800

    add forgot password page

--- src/routes/_auth/forgot-password.tsx
+++ src/routes/_auth/forgot-password.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ForgotPasswordForm from "./-components/forgot-password-form";
 
 export const Route = createFileRoute("/_auth/forgot-password")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_auth/forgot-password"!</div>;
+  return <ForgotPasswordForm />;
 }
